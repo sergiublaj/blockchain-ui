766eacf87e620b01c7f05c29cd5bf9e1337baa85
sergiumr <s3rgiumr@gmail.com>
2023-03-09 15:37:19 +0200

    changed contract address

--- src/app/core/web3.ts
+++ src/app/core/web3.ts
@@ -1,4 +1,7 @@
 import Web3 from 'web3';
 
 export const web3 = new Web3((window as any).ethereum);
-export const auctionContractAddress = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+
+// found in ./contracts/AwesomePictureAuctions.json in network/address
+// after running truffle migrate for solidity contracts
+export const auctionContractAddress = '0x381445710b5e73d34aF196c53A3D5cDa58EDBf7A';
